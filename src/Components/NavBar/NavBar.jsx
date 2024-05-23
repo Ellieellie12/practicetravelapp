71004f1948b6bca7e2d88be4b0e7a0ae96f6b55e
Ellie <eagyeman1234@gmail.com>
2024-05-23 15:32:45 -0400

    add links for navbar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -14,6 +14,31 @@ function NavBar() {
               <h1> <MdOutlineTravelExplore className='icon'/> Travel.</h1>
             </a>
           </div>
+          <div className='navBar'>
+            <ul className="navList flex">
+              <li className="navitem">
+                <a href="#" className='navLink'>Home</a>
+              </li>
+              <li className="navitem">
+                <a href="#" className='navLink'>Packages</a>
+              </li>
+              <li className="navitem">
+                <a href="#" className='navLink'>Shop</a>
+              </li>
+              <li className="navitem">
+                <a href="#" className='navLink'>About</a>
+              </li>
+              <li className="navitem">
+                <a href="#" className='navLink'>Pages</a>
+              </li>
+              <li className="navitem">
+                <a href="#" className='navLink'>News</a>
+              </li>
+              <li className="navitem">
+                <a href="#" className='navLink'>Contact</a>
+              </li>
+            </ul>
+          </div>
         </header>
       </section>
     </>
