a229ada85aa2105023862b8a0e0c8e42a62e196e
Ellie <eagyeman1234@gmail.com>
2024-05-23 15:35:29 -0400

    add button for bookings

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -14,31 +14,43 @@ function NavBar() {
               <h1> <MdOutlineTravelExplore className='icon'/> Travel.</h1>
             </a>
           </div>
+
           <div className='navBar'>
-            <ul className="navList flex">
+            <ul className="navLists flex">
               <li className="navitem">
                 <a href="#" className='navLink'>Home</a>
               </li>
+
               <li className="navitem">
                 <a href="#" className='navLink'>Packages</a>
               </li>
+
               <li className="navitem">
                 <a href="#" className='navLink'>Shop</a>
               </li>
+
               <li className="navitem">
                 <a href="#" className='navLink'>About</a>
               </li>
+
               <li className="navitem">
                 <a href="#" className='navLink'>Pages</a>
               </li>
+
               <li className="navitem">
                 <a href="#" className='navLink'>News</a>
               </li>
+
               <li className="navitem">
                 <a href="#" className='navLink'>Contact</a>
               </li>
+
+              <button className='btn'>
+                <a href="#">BOOK NOW</a>
+              </button>
             </ul>
           </div>
+
         </header>
       </section>
     </>
